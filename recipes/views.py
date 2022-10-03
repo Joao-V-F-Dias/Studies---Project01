c459a749a8cf3f734a29846b7297b8d4684314eb
Joao-V-F-Dias <jvfdias@gmail.com>
2022-10-03 15:42:42 -0300

    Removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={'nome': 'João Dias'})
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html', context={})
-
-
-def contato(request):
-    return HttpResponse('Contato 2')
