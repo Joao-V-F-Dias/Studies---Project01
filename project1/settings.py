531232f5c0d0cac64c6f5444a45268bc515286f0
Joao-V-F-Dias <jvfdias@gmail.com>
2022-10-03 15:26:49 -0300

    Further changes with studying purposes

--- project1/settings.py
+++ project1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base_templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
